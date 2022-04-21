eea933c6ca364aa9bcaceed0df3ee3970efc7901
cmlimm <cmothlimm@gmail.com>
2022-04-22 00:53:16 +0300

    solve armstrong-numbers exercise

---
+++ armstrong-numbers/src/armstrong_numbers.clj
@@ -0,0 +1,18 @@
+(ns armstrong-numbers)
+
+(defn pow 
+  [x n]
+  (reduce * (repeat n x)))
+
+(defn armstrong?
+  "Returns `true` if `num` is an [Armstrong number](https://en.wikipedia.org/wiki/Narcissistic_number)"
+  [num]
+  (let [digits (map #(mod % 10) 
+                    (take-while pos? 
+                                (iterate #(quot % 10) 
+                                         num)))
+        n (count digits)]
+    (== num 
+        (reduce + 
+                (map #(pow % n) 
+                     digits)))))
